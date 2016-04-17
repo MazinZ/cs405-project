843ffb1d089e71b4fdc92800464695d958ca1166
wilcoxec <erika.wilcox@uky.edu>
2016-04-17 18:52:35 -0400

    added shopping

---
+++ shopping/display.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+require('../database.php');
+
+
+
+function getCount($item_id, $email)
+{
+    $res = $pdo->prepare('SELECT COUNT(*) as count FROM Carts WHERE item_id = ? AND password = ?');
+    $testVal = $res->execute(array($item_id, $email));
+
+    $row = $res->fetch();
+    return $row['count'];
+}
+
+
+function displayItems($PStatment, $returnU, $USE_FLAG)
+{
+    
+}
+
+?>
