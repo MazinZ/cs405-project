8b1446add1cbbf028417f9a48eed305cbbc27fb1
wilcoxec <erika.wilcox@uky.edu>
2016-04-18 13:37:22 -0400

    test

--- shopping/display.php
+++ shopping/display.php
@@ -6,7 +6,6 @@ session_start();
 function displayItems($conn)
 {
 	require("addItemToCart.php");
-
 	$allcartItemsQuery = mysqli_query($conn, "SELECT * FROM Items");
 
 	echo '<tr>';
@@ -15,7 +14,7 @@ function displayItems($conn)
 			echo '<td>' .$res['name']. '</td>' ;
 			echo '<td>' .$res['description']. '</td>' ;
 			echo '<td>' .$res['price']. '</td>' ;
-			echo '<td><a href="addItemToCart.php">Add Item to Cart</a></td>';
+			echo '<td><a class\"addToCartLink\" href=\"#\"$addToCartItem\">Add Item to Cart</a></td>';
 			echo '<br>';
 		endforeach;
 	echo '</tr>';
