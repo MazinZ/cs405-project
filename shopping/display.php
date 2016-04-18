e79a1278d6117edf882f19aadc12088801b65260
wilcoxec <erika.wilcox@uky.edu>
2016-04-18 12:08:53 -0400

    testing

--- shopping/display.php
+++ shopping/display.php
@@ -3,23 +3,19 @@
 require('../database.php');
 
 
-
-
-
-
 function displayItems()
 {
 	$allcartItemsQuery = mysqli_query($conn, "SELECT * FROM Items");
 
+	echo .$allcartItemsQuery.;
 
-	echo '<tr>';
-		foreach ($allcartItemsQuery as $res ):
-			echo '<td>' .$res['name']. '</td>' ;
-			echo '<td>' .$res['description']. '</td>' ;
-			echo '<td>' .$res['price']. '</td>' ;
-
-		endforeach;
-	echo '</tr>';
+	//echo '<tr>';
+	//	foreach ($allcartItemsQuery as $res ):
+	//		echo '<td>' .$res['name']. '</td>' ;
+	//		echo '<td>' .$res['description']. '</td>' ;
+	//		echo '<td>' .$res['price']. '</td>' ;
+	//	endforeach;
+	//echo '</tr>';
 	
 
 }
@@ -27,16 +23,16 @@ function displayItems()
 
 function displayCart($email)
 {
-	$cartItemsQuery = mysqli_query($conn, "SELECT * FROM Cart WHERE email ='".email."'");
+	//$cartItemsQuery = mysqli_query($conn, "SELECT * FROM Cart WHERE email ='".email."'");
 
-	echo '<tr>';
-		foreach ($cartItemsQuery as $res ):
-			echo '<td>' .$res['name']. '</td>' ;
-			echo '<td>' .$res['description']. '</td>' ;
-			echo '<td>' .$res['price']. '</td>' ;
+	//echo '<tr>';
+	//	foreach ($cartItemsQuery as $res ):
+	//		echo '<td>' .$res['name']. '</td>' ;
+	//		echo '<td>' .$res['description']. '</td>' ;
+	//		echo '<td>' .$res['price']. '</td>' ;
 
-		endforeach;
-	echo '</tr>';
+	//	endforeach;
+	//echo '</tr>';
 }
 
 ?>
