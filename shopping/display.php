b9284e82eca79396f12e4bb86e9ca57e327e0c70
wilcoxec <erika.wilcox@uky.edu>
2016-04-18 18:44:46 -0400

    changed display cart

--- shopping/display.php
+++ shopping/display.php
@@ -28,15 +28,16 @@ function displayCart($email)
 	require("removeItemFromCart.php");
 
 	//$cartItemsQuery = mysqli_query($conn, "SELECT item_id FROM Cart WHERE email='."$email".'");
-	$cartQuery = mysqli_query($conn, "SELECT item_id FROM Cart WHERE email ='".$email."'");
+	$cartQuery = mysqli_query($conn, "SELECT * FROM Cart C, Items I WHERE I.item_id = C.item_id AND email ='".$email."'");
+
 
 
 	echo '<tr>';
 		foreach ($cartQuery as $res ):
 			$removeFromCartItem = "removeItemFromCart.php?item_id=" .$res["item_id"];
-			echo '<td>' .$res['cart_id']. '</td>' ;
-			echo '<td>' .$res['email']. '</td>' ;
-			echo '<td>' .$res['item_id']. '</td>' ;
+			echo '<td>' .$res['name']. '</td>' ;
+			echo '<td>' .$res['description']. '</td>' ;
+			echo '<td>' .$res['price']. '</td>' ;
 			echo '<td><a href='.$removeFromCartItem.'>Remove Item</a></td>';
 			echo '<br>';
 		endforeach;
--- shopping/removeItemFromCart.php
+++ shopping/removeItemFromCart.php
@@ -5,8 +5,7 @@
 	
 	$item_ID = $_GET['item_id'];
 	$email = $_SESSION['currUserEmail'];
-	echo $item_ID;
-	echo $email;
+
 
 	$cartQuery = mysqli_query($conn,"DELETE FROM Cart WHERE email ='".$email."' AND item_id ='".$item_ID."'");
 
