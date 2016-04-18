84cef102b8d88ae78e1f09547f58239a09c8ac71
wilcoxec <erika.wilcox@uky.edu>
2016-04-18 17:34:10 -0400

    added query to add cart

--- shopping/addItemToCart.php
+++ shopping/addItemToCart.php
@@ -10,7 +10,9 @@
 
 
 	$item_ID = $_GET['item_id'];
-
+	$email = $_SESSION['currUserEmail'];
+	echo $item_ID;
+	echo $email;
 
 
 	$cartQuery = mysqli_query($conn, "INSERT INTO Cart(email, item_id) VALUES(?, ?)");
