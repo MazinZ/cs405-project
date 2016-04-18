ad09ee4a5591e6ce3b168dfbf2eed64c084ae970
Mazin Zakaria <mazinzakaria1@gmail.com>
2016-04-18 18:50:34 -0400

    View Cart added to navbar

--- templates/topBar.php
+++ templates/topBar.php
@@ -5,11 +5,31 @@
 
    		 <li class="nav-item active">
 
-     		 <a class="nav-link" href="<?php echo HTTP . "logout.php"?>">Logout <span class="sr-only">(current)</span></a>
-      
-      <?php } ?>
-    </li>
-    
+     		 <a class="nav-link" href="<?php echo HTTP . "logout.php"?>">Logout </a>
+         </li>
 
+      <?php } ?>
+    
+   	  <?php if (!isset($_SESSION["loggedIn"])){ ?>
+
+   		 <li class="nav-item active">
+
+     		 <a class="nav-link" href="<?php echo HTTP . "login/index.php"?>">Login</a>
+         </li>
+
+      <?php } ?>
+    
+	
+    
+     <?php if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] == true && isset($_SESSION["userType"]) && $_SESSION["userType"] == 0){ ?>
+
+   		 <li class="nav-item active">
+
+     		 <a class="nav-link" href="<?php echo HTTP . "shopping/displayCart.php"?>">View Cart </a>
+         </li>
+
+      <?php } ?>
+      
+      
   </ul>
   </nav>
