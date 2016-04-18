d901571687988e9ac99ee8a0b2445474e9fdc9ae
Mazin Zakaria <mazinzakaria1@gmail.com>
2016-04-18 19:01:37 -0400

    Added topBar to home

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
 <head>
 <meta charset="UTF-8">
 <title>Home</title>
+    <?php  include_once "./templates/includes.php"; ?>
+
 </head>
 
 <body>
+	<h1> Home </h1>
+    <?php  include_once "./templates/topBar.php"; ?>
 
-
-	<a href="./login">Login</a>
-	<a href="./logout.php">Logout</a>
+	<!--<a href="./login">Login</a>
+	<a href="./logout.php">Logout</a>-->
     <br>
         <br>
 
