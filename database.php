84917ae32832a76e65f45d9213973657ec09c497
wilcoxec <erika.wilcox@uky.edu>
2016-04-18 13:01:44 -0400

    test

--- database.php
+++ database.php
@@ -1,5 +1,4 @@
 <?php 
-
 	$username = "root";
 	$password = "";
 	$dbname = "cs405";
@@ -7,10 +6,8 @@
 	
 	$conn = new mysqli($servername, $username, $password, $dbname)
 		or die("unable to connect");
-
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	} 
-
 	mysql_select_db('cs405');
 ?>
