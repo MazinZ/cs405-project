80efee00679e1accb4391ceffb7d78bb2da649ce
wilcoxec <erika.wilcox@uky.edu>
2016-04-18 10:43:33 -0400

    Add item to cart

--- shopping/addItemToCart.php
+++ shopping/addItemToCart.php
@@ -1,7 +1,16 @@
 <?php
-	session_start();
-
+	
 	require('../database.php');
+
+	if(!isset($_SESSION['currUserEmail']))
+	{
+		header("Location: ../login/index.php");
+		die(0);
+	}
+
+	$item_ID = $_GET['item_id'];
+
+	$cartQuery = mysqli_query($conn, "INSERT INTO Cart(email, item_id) VALUES(?, ?)");
 
 
 
