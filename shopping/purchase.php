83b5a79a32a8ca8b99d9ca1fb3e71fdf439fdc41
wilcoxec <erika.wilcox@uky.edu>
2016-04-18 23:04:05 -0400

    fixed issue with customer order

--- shopping/purchase.php
+++ shopping/purchase.php
@@ -6,6 +6,7 @@
 
 	$orderQuery = mysqli_query($conn, "SELECT COUNT(DISTINCT order_id) + 1 as id FROM Orders");
 
+	//Get the order id
 	$orderID =  $orderQuery->fetch_object()->id;
 
 	$orderCon = mysqli_query($conn, "INSERT INTO Orders(status, order_date, ship_date) VALUES (0, NOW(), NULL)");
@@ -14,18 +15,19 @@
 	$cartCount = mysqli_query($conn, "SELECT COUNT(*) as cnt FROM Cart WHERE email ='".$email."'");
 	$numItems = $cartCount->fetch_object()->cnt;
 
-	echo $numItems;
-
+	//Get items from the cart
 	$cartQuery = mysqli_query($conn, "SELECT * FROM Cart C, Items I WHERE I.item_id = C.item_id AND C.email ='".$email."'");
 
+	//Create Item Orders
 	foreach ($cartQuery as $row ): 
 		$itemID = $row['item_id'];
 		$itemOrder = mysqli_query($conn, "INSERT INTO ItemOrders(order_id, item_id) VALUES($orderID, $itemID)");
 		$updateStock = mysqli_query($conn, "UPDATE Items SET stock = (stock - 1) WHERE item_id ='".$itemID."'");		
 	endforeach;
 
-	//THIS PART DOES NOT WORK!!!!
-	$custOrder = mysqli_query($conn, "INSERT INTO CustomerOrders (email, order_id) VALUES($email, $orderID)");
+	//Create Customer Order	
+	$custOrder = mysqli_query($conn, "INSERT INTO CustomerOrders (email, order_id) VALUES('$email', $orderID)");
+	
 
 	$deleteCart = mysqli_query($conn, "DELETE FROM Cart WHERE email ='".$email."'");
 
