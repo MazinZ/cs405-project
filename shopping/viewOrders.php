087cfe772da68df346e0f545e9df35799d8c68cd
wilcoxec <erika.wilcox@uky.edu>
2016-04-19 22:10:20 -0400

    fixed

--- shopping/viewOrders.php
+++ shopping/viewOrders.php
@@ -1,5 +1,6 @@
 <?php require('../database.php'); 
 session_start();
+$email = $_SESSION['currUserEmail'];
 ?>
 
 <!doctype html>
@@ -14,19 +15,19 @@ session_start();
 
 	<h1>Orders</h1>
 	<?php  include_once "../templates/topBar.php"; ?>
- 	<?php
-    	require("../database.php");
-    ?>
     
     <?php
 
 
-	$email = $_SESSION['currUserEmail'];
+	
 
 	$custOrdersQuery = mysqli_query($conn, "SELECT O.order_id as order_id, O.order_date as order_date, O.status as status FROM Orders O, CustomerOrders CO WHERE O.order_id = CO.order_id AND CO.email ='".$email."'");
 	
 	foreach ($custOrdersQuery as $row ):
 		$orderID = $row['order_id'];
+		//$orderStatus = $row['status'];
+
+		//echo $orderStatus;
 		echo"<p> Item Order Date: ".$row['order_date']."</br></p>";
 		echo"<p> Order Status: ".$row['status']."</br></p>";
 
