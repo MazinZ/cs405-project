6eb89a0b4f4f41023b8747d9448a93ef3c807fc9
wilcoxec <erika.wilcox@uky.edu>
2016-04-19 21:24:39 -0400

    made changes to viewOrders.php

--- shopping/viewOrders.php
+++ shopping/viewOrders.php
@@ -24,35 +24,23 @@ session_start();
 	$email = $_SESSION['currUserEmail'];
 
 	$custOrdersQuery = mysqli_query($conn, "SELECT O.order_id, O.order_date, O.status FROM Orders O, CustomerOrders CO WHERE O.order_id = CO.order_id AND CO.email ='".$email."'");
-	echo mysqli_error($conn);
-
+	
 	foreach ($custOrdersQuery as $row ):
 		$orderID = $row['order_id'];
 		echo"<p> Item Order Date: ".$row['order_date']."</br></p>";
 		echo"<p> Order Status: ".$row['status']."</br></p>";
 
-		$itemsOrdersQuery = mysqli_query($conn, "SELECT I.name, I.description 
+		/*$itemsOrdersQuery = mysqli_query($conn, "SELECT I.name, I.description 
 									FROM Items I, ItemOrders IO
 									WHERE I.item_id = IO.item_id AND IO.order_id = '".$orderID."'");
-
+		
 		foreach ($itemsOrdersQuery as $res ):
         	echo"<p> Item Name: ".$res['name']."</br></p>";
         	echo"<p> Item Description: ".$res['description']."</br></p>";		
-		endforeach;    		
+		endforeach; */   		
 	
 	endforeach;
-
-
 	
-
-	
-
-
-
-
-	
-	//header("location:../shopping/confirmOrder.php");
-	exit();
 
 ?>
     
