87b221af76b3caea8722fccb66dad745f6da6778
wilcoxec <erika.wilcox@uky.edu>
2016-04-19 22:16:58 -0400

    f

--- shopping/viewOrders.php
+++ shopping/viewOrders.php
@@ -18,26 +18,21 @@ $email = $_SESSION['currUserEmail'];
     
     <?php
 
-
-	
-
 	$custOrdersQuery = mysqli_query($conn, "SELECT O.order_id as order_id, O.order_date as order_date, O.status as status FROM Orders O, CustomerOrders CO WHERE O.order_id = CO.order_id AND CO.email ='".$email."'");
-	
+	echo mysqli_error($conn); 
 	foreach ($custOrdersQuery as $row ):
-		$orderID = $row['order_id'];
-		//$orderStatus = $row['status'];
-
-		//echo $orderStatus;
 		echo"<p> Item Order Date: ".$row['order_date']."</br></p>";
 		echo"<p> Order Status: ".$row['status']."</br></p>";
 
 		$itemsOrdersQuery = mysqli_query($conn, "SELECT I.name, I.description 
 									FROM Items I, ItemOrders IO
 									WHERE I.item_id = IO.item_id AND IO.order_id = '".$orderID."'");
+		echo mysqli_error($conn); 
 		
 		foreach ($itemsOrdersQuery as $res ):
         	echo"<p> Item Name: ".$res['name']."</br></p>";
-        	echo"<p> Item Description: ".$res['description']."</br></p>";		
+        	echo"<p> Item Description: ".$res['description']."</br></p>";
+
 		endforeach;   		
 	
 	endforeach;
